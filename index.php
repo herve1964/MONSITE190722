af612b8d136abbc8e8ba041c80fc7cddae39611f
Shadow <fernandes.herve@gmail.com>
2022-07-19 16:19:36 +0200

    avec debut texte bar partition V11

--- index.php
+++ index.php
@@ -57,31 +57,7 @@
 </nav>
 
 
-<!-- 
-<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Partitions</a>
-    </div>
-      <ul class="nav navbar-nav"> 
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-  </div>
-</nav>
- -->
+
 <div class="container">
 	<div class="row">
 		<div class="col-sm-6 col-md-7 col-lg-6 text-center bloc-img-description">
