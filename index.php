32b6573912d03317aa768a34a8704d7e0f90f8a6
herve1964 <92846840+herve1964@users.noreply.github.com>
2022-07-19 15:05:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,155 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  
+  <!-- Balises Meta obligatoires! -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Ma page</title>
+
+	<!-- Appel du css Bootstrap -->	
+    <link href="css/bootstrap.css" rel="stylesheet">
+    
+    <link href="css/custom.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+
+  <body>
+
+
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#partie-reduite" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="partie-reduite">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<!-- 
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+  </div>
+</nav>
+ -->
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6 col-md-7 col-lg-6 text-center bloc-img-description">
+			<h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+			<p class="hidden-xs hidden-sm"> Etiam auctor enim eu libero varius, id tincidunt dolor iaculis. Aenean iaculis enim nec nulla sagittis, ut gravida felis tristique. Suspendisse rutrum neque quam, nec elementum tellus condimentum sit amet. Aliquam rhoncus fermentum leo vel pulvinar.</p>
+			<a href="#" class="btn btn-lg btn-warning">Commandez Maintenant</a>
+		</div>
+		<div class="col-sm-6 col-md-5 col-lg-6">
+			<center><img src="img/img-madame.jpg" class="img-responsive" alt="Responsive image"></center>
+		</div>
+	</div>
+</div>
+
+<!-- 
+<div class="container">
+    <h1>Hello, world!</h1>
+</div>
+ -->
+    
+<!-- 
+    <div class="row">
+    	<div class="bloc col-md-6 col-sm-8">
+    	
+    	</div>
+    	<div class="bloc col-md-6 col-sm-4">
+    	
+    	</div>
+    </div>
+ -->
+ 
+<!-- 
+ <div class="container">   
+    <div class="row">
+    	<div class="bloc col-md-4 hidden-xs hidden-sm">
+    		bloc 1
+    	</div>
+    	<div class="bloc col-md-4">
+    		bloc 2
+    	</div>
+    	<div class="bloc col-md-4 hidden-xs hidden-sm">
+    		bloc 3
+    	</div>
+
+    </div>   
+</div>
+ -->
+ 
+<!-- 
+<div class="container">   
+    <div class="row">
+    	<div class="bloc col-md-4">
+    		<p class="responsive-font">Bloc 1</p>
+    	</div>
+    	<div class="bloc col-md-4">
+    		<p class="responsive-font">Bloc 2</p>
+    	</div>
+    	<div class="bloc col-md-4">
+    		<p class="responsive-font">Bloc 3</p>
+    	</div>
+
+    </div>   
+</div>
+ -->
+
+	<!-- Appel du js Bootstrap -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+
+</html>
  
