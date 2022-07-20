8da54964c53608cdc0b21e33a82f6e59dc9adc79
Shadow <fernandes.herve@gmail.com>
2022-07-20 10:05:50 +0200

    avec debut image avec lien accordeon V122

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-  
-  <!-- Balises Meta obligatoires! -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Ma page</title>
-
-	<!-- Appel du css Bootstrap -->	
-    <link href="css/bootstrap.css" rel="stylesheet">
-    
-    <link href="css/custom.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-  </head>
+<?php
+include('includes/base.php');
+?>  
 
   <body>
-
 
 <nav class="navbar navbar-default">
   <div class="container">
@@ -61,7 +44,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-6 col-md-7 col-lg-6 text-center bloc-img-description">
-			<h1>Ceci est un site de test en cours V121</h1>
+			<h1>Ceci est un site de test en cours V122</h1>
 			<p class="hidden-xs hidden-sm"> Mes liste de partitions sont en cours .....</p>
 			<a href="#" class="btn btn-lg btn-warning">Commandez Maintenant</a>
 		</div>
