3781ac8b77f7d67b028ba2b43590f14f9f27b09d
Shadow <fernandes.herve@gmail.com>
2022-07-20 10:43:25 +0200

    avec debut image avec lien accordeon V124

--- index.php
+++ index.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-include('includes/base.php');
-?>  
+<?php include('includes/base.php'); ?>
 
 <body>
-<?php
-include('includes/navbar.php');
-?>  
+	<?php include('includes/navbar.php'); ?>
 
 
-<div class="container">
-	<div class="row">
-		<div class="col-sm-6 col-md-7 col-lg-6 text-center bloc-img-description">
-			<h1>Ceci est un site de test en cours V123</h1>
-			<p class="hidden-xs hidden-sm"> Mes liste de partitions sont en cours .....</p>
-			<a href="#" class="btn btn-lg btn-warning">Commandez Maintenant</a>
-		</div>
-		<div class="col-sm-6 col-md-5 col-lg-6">
-			<center><img src="img/Partitions.jpg" class="img-responsive" alt="Responsive image"></center>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-md-7 col-lg-6 text-center bloc-img-description">
+				<h1>Ceci est un site de test en cours V124/h1>
+					<p class="hidden-xs hidden-sm"> Mes liste de partitions sont en cours .....</p>
+					<a href="#" class="btn btn-lg btn-warning">Commandez Maintenant</a>
+			</div>
+			<div class="col-sm-6 col-md-5 col-lg-6">
+				<center><img src="img/Partitions.jpg" class="img-responsive" alt="Responsive image"></center>
+			</div>
 		</div>
 	</div>
-</div>
 
-<?php
-include('includes/snippets.php');
-?>
+	<?php
+	include('includes/snippets.php');
+	?>
 </body>
+
 </html>
- 
