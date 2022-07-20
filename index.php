d7b9b35eb105e6c9676ccf7cd00d19463f6c41d7
Shadow <fernandes.herve@gmail.com>
2022-07-20 10:44:43 +0200

    avec debut image avec lien accordeon V124

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-6 col-md-7 col-lg-6 text-center bloc-img-description">
-				<h1>Ceci est un site de test en cours V124/h1>
-					<p class="hidden-xs hidden-sm"> Mes liste de partitions sont en cours .....</p>
-					<a href="#" class="btn btn-lg btn-warning">Commandez Maintenant</a>
+				<h1>Ceci est un site de test en cours V124</h1>
+				<p class="hidden-xs hidden-sm"> Mes liste de partitions sont en cours .....</p>
+				<a href="#" class="btn btn-lg btn-warning">Commandez Maintenant</a>
 			</div>
 			<div class="col-sm-6 col-md-5 col-lg-6">
 				<center><img src="img/Partitions.jpg" class="img-responsive" alt="Responsive image"></center>
