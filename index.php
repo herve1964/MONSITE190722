f361cc64ab3e31080f3c340669e34ee01e1d3a84
Shadow <fernandes.herve@gmail.com>
2022-07-20 09:30:54 +0200

    avec debut image avec lien accordeon V12

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Partitions</a>
+      <a class="navbar-brand" href="https://www.partitions-accordeon.com">Partitions</a>
     </div>
 
     <div class="collapse navbar-collapse" id="partie-reduite">
