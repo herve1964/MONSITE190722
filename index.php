530aa685ef2968b49683ab6990d179535fd76101
Shadow <fernandes.herve@gmail.com>
2022-07-20 11:18:16 +0200

    avec debut image avec mather V126

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include('includes/base.php');
+<?php
+
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+include('includes/base.php');
 require __DIR__ . '/vendor/autoload.php';
+$listeRoute = new Route('/');
+$createRoute = new Route('/Create');
+$showRoute = new Route('show');
+
+$collection = new RouteCollection();
+$collection->add('list', $listeRoute);
+$collection->add('create', $createRoute);
+$collection->add('show', $showRoute);
+
+$matcher = new UrlMatcher($collection, new RequestContext());
+$resultat = $matcher->match('/');
+var_dump($resultat);
+die();
 ?>
 
 <body>
@@ -11,7 +31,7 @@ require __DIR__ . '/vendor/autoload.php';
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-6 col-md-7 col-lg-6 text-center bloc-img-description">
-				<h1>Ceci est un site de test en cours V125</h1>
+				<h1>Ceci est un site de test en cours V126</h1>
 				<p class="hidden-xs hidden-sm"> Mes liste de partitions sont en cours .....</p>
 				<a href="#" class="btn btn-lg btn-warning">Commandez Maintenant</a>
 			</div>
