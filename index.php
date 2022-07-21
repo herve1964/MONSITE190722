c8fd49b492ac7624a4795f63e4ec991c6dad3d79
Shadow <fernandes.herve@gmail.com>
2022-07-21 13:23:27 +0200

    avec debut image avec mather V126

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+require __DIR__ . '/vendor/autoload.php';
 
 
 
@@ -20,9 +21,14 @@ $collection->add('show', $showRoute);
 
 $matcher = new UrlMatcher($collection, new RequestContext());
 
+//dd($_SERVER);
 $pathInfo = $_SERVER['PATH_INFO']  ?? '/';
 
+dump($pathInfo);
 
 
 $resultat = $matcher->match($pathInfo);
-dump($pathInfo);
+
+dump($resultat);
+$page = $resultat['_route'];
+require_once "pages/$page.php";
