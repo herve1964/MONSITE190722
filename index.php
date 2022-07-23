f4fb7d54a4139fc91760336ee10d9720467a7f38
Shadow <fernandes.herve@gmail.com>
2022-07-23 11:30:54 +0200

    avec debut image avec mather htacces

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
 
 use Symfony\Component\Routing\Route;
@@ -5,30 +7,49 @@ use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 
+include('includes/base.php');
 require __DIR__ . '/vendor/autoload.php';
-
-
-
-$listRoute = new Route('/');
+$listeRoute = new Route('/');
 $createRoute = new Route('/create');
 $showRoute = new Route('/show');
 
 $collection = new RouteCollection();
-$collection->add('list', $listRoute);
+$collection->add('list', $listeRoute);
 $collection->add('create', $createRoute);
 $collection->add('show', $showRoute);
 
 
 $matcher = new UrlMatcher($collection, new RequestContext());
 
-dump($collection);
-$pathInfo = $_SERVER['PATH_INFO']  ?? '/';
-
-dump($pathInfo);
-
+//$pathInfo = $_SERVER->match['PATH_INFO'] ?? '/';
 
 $resultat = $matcher->match($pathInfo);
 
-dump($resultat);
-$page = $resultat['_route'];
-require_once "pages/$page.php";
+
+var_dump($resultat);
+die();
+?>
+
+<body>
+	<?php include('includes/navbar.php'); ?>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-md-7 col-lg-6 text-center bloc-img-description">
+				<h1>Ceci est un site de test en cours V126</h1>
+				<p class="hidden-xs hidden-sm"> Mes liste de partitions sont en cours .....</p>
+				<a href="#" class="btn btn-lg btn-warning">Commandez Maintenant</a>
+			</div>
+			<div class="col-sm-6 col-md-5 col-lg-6">
+				<center><img src="img/Partitions.jpg" class="img-responsive" alt="Responsive image"></center>
+			</div>
+		</div>
+	</div>
+
+	<?php
+	include('includes/snippets.php');
+	?>
+</body>
+
+</html>
